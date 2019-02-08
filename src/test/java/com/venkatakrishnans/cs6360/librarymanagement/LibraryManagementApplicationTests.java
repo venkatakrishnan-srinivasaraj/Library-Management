fe689e5542b2b873b59d1b1ae6c7c8e1c591342d
Venkatakrishnan Srinivasaraj <venkatakrishnan111@gmail.com>
2019-02-07 20:36:13 -0600

    Commiting missed files

--- src/test/java/com/venkatakrishnans/cs6360/librarymanagement/LibraryManagementApplicationTests.java
+++ src/test/java/com/venkatakrishnans/cs6360/librarymanagement/LibraryManagementApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LibrarymanagementApplicationTests {
+public class LibraryManagementApplicationTests {
 
     @Test
     public void contextLoads() {
